bbe7f1d370d5908d9b915b69dde5c9f397dbe9a7
Михаил <168448663+11Misha@users.noreply.github.com>
2024-05-05 22:20:10 +0800

    Add files via upload

---
+++ homevork3.py
@@ -0,0 +1,9 @@
+name = "my_string"
+print(name)
+print(len(name))
+name = "my_string"
+print("my_string".upper())
+print("my_string".lower())
+print("my_string".replace("_",""))
+print(name[0])
+print(name[8])
